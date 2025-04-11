8aaf584d3391c3d2fb2d481d6ea69cef51ae06fe
himanshudbg <boost.xd.02@gmail.com>
2025-04-11 19:24:34 +0530

    update

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,12 +5,11 @@ do
     echo "Waiting for server volume..."
 done
 
-
 until python manage.py migrate
 do
     echo "Waiting for db to be ready..."
     sleep 2
 done
 
-
-gunicorn --bind :8000 --workers 2 djproject.wsgi
+# Use the correct WSGI application path for your Django project
+gunicorn --bind 0.0.0.0:$PORT --workers 2 floorhosting.wsgi
