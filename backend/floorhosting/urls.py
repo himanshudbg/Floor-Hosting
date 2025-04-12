981caf80d2ac950d714b99a0a3c3042b30f1d358
himanshudbg <boost.xd.02@gmail.com>
2025-04-12 14:32:41 +0530

    change

--- backend/floorhosting/urls.py
+++ backend/floorhosting/urls.py
@@ -15,7 +15,7 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include, re_path
 from django.conf import settings
 from django.conf.urls.static import static
 from django.views.generic import TemplateView
@@ -23,8 +23,8 @@ from django.views.generic import TemplateView
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('gameservers.urls')),
-    path('', TemplateView.as_view(template_name='index.html')),
+    # Serve static files
+    *static(settings.STATIC_URL, document_root=settings.STATIC_ROOT),
+    # This should be the last pattern - it will catch all other URLs and serve the React app
+    re_path(r'^.*$', TemplateView.as_view(template_name='index.html')),
 ]
-
-# Serve static files in all environments, not just DEBUG
-urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
